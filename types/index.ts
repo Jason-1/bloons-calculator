80ae637186ff83111fd8b4bf3151db714318dda4
Jason-1 <jason722698@gmail.com>
2025-05-21 16:47:48 +1200

    initial commit

---
+++ types/index.ts
@@ -0,0 +1,21 @@
+export type monkey = {
+  name: string;
+  easy: pathPrices;
+  medium: pathPrices;
+  hard: pathPrices;
+  impoppable: pathPrices;
+};
+
+export type pathPrices = {
+  top: prices;
+  middle: prices;
+  bottom: prices;
+};
+
+export type prices = {
+  1: number;
+  2: number;
+  3: number;
+  4: number;
+  5: number;
+};
